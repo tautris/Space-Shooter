c0bc2f803223325404bcb739ebc819e42bb4c371
Tautrimas Gumbys <tautrimas.g2@gmail.com>
2018-09-10 21:55:56 +0300

    Add boundary + tilt

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Diagnostics;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
 
 public class PlayerController : MonoBehaviour
 {
+    public Boundary boundary;
 
     private Rigidbody rigidBody;
 
     private void Start()
     {
         rigidBody = gameObject.GetComponent<Rigidbody>();
-
     }
+    public float speed;
+    public float tilt;
     private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rigidBody.velocity = movement;
+        rigidBody.velocity = movement * speed;
+
+        rigidBody.position = new Vector3
+        (
+            Mathf.Clamp (rigidBody.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp (rigidBody.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rigidBody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidBody.velocity.x * -tilt);
     }
 }
