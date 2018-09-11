06bebd6e42b849d69860c71267ff90705ab36e2f
Tautrimas Gumbys <tautrimas.g2@gmail.com>
2018-09-11 23:03:11 +0300

    Add explosions + asteroid spawns

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Boundary")
         {
             return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
         }
         Destroy(other.gameObject);
         Destroy(gameObject);
